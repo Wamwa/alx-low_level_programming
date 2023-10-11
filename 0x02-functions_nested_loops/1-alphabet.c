b88bfb73cda6f2c592772f1f32ee30843530a428
Wamwa <chriskhatiala@gmail.com>
2023-10-11 08:12:08 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_alphabet - task function
+  * Return: return 0
+  */
+void print_alphabet(void)
+{
+
+char c;
+for (c= 'a'; c <= '2'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
